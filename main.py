b8e14cf60469ac5006757bb220b62619450dede5
Haider Nasir <haider.nasir6462@gmail.com>
2024-09-03 12:22:56 +0100

    use main file to call functions

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ if __name__ == "__main__":
     converted_text = convert_wav_to_text(wav_file_path)
     print("Transcribed Text:", converted_text)
 
-api_response = generate_response(converted_text)
+if converted_text: 
+    api_response = generate_response(converted_text)
 
 if api_response:
     print("API Response:", api_response)
-    # You can access the generated response from the API response
-    generated_text = api_response["generated_text"]
-    print("Generated Text:", generated_text)
